383f56371ae6581482075de534691bbe0f275dd5
Mario Tutek <m.tutek@gmail.com>
2021-03-05 20:24:41 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,15 +1,15 @@
 function basicTeenager(16) {
-  if (age > 13 && age < 19) {
+  if (age > 13 && 19) {
     return ("You are a teenager!")
 }
 }
 
 function teenager(age) {
-  
+
 }
 
 function ageChecker(age) {
-  
+
 }
 
 function ternaryTeenager(age) {
